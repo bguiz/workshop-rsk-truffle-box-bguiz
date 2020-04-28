d382e68fe2cf90d1dd0f1a153f720d7571ec57b3
bguiz <bguiz@users.noreply.github.com>
2020-04-28 21:12:42 +0800

    step: 06-02: test for adopt function

--- test/adoption.test.js
+++ test/adoption.test.js
@@ -1,4 +1,26 @@
 const Adoption = artifacts.require('Adoption');
 
+const assert = require('assert');
+
+const BN = web3.utils.BN;
+
 contract('Adoption', (accounts) => {
+  it('user can adopt pet', async () => {
+    const inst = await Adoption.deployed();
+
+    const adopterAccount = accounts[5];
+    const expectedPetId = new BN(8);
+    const adoptTxInfo = await inst.adopt(
+      expectedPetId,
+      {
+        from: adopterAccount,
+      },
+    );
+
+    assert.equal(
+      adoptTxInfo.receipt.status,
+      true,
+      'adoption transaction failed',
+    );
+  });
 });
