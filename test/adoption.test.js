dc4e46771e2d1a513707fadafdabfd2780df0374
bguiz <bguiz@users.noreply.github.com>
2020-04-28 21:13:36 +0800

    step: 06-03: test for state change after adopt function called

--- test/adoption.test.js
+++ test/adoption.test.js
@@ -23,4 +23,20 @@ contract('Adoption', (accounts) => {
       'adoption transaction failed',
     );
   });
+
+  it('remembers adopter account', async () => {
+    inst = await Adoption.deployed();
+
+    const adopterAccount = accounts[5];
+    const expectedPetId = new BN(8);
+    const returnedAccount = await inst.adopters(
+      expectedPetId,
+    );
+
+    assert.equal(
+      returnedAccount,
+      adopterAccount,
+      'returned adopter account mismatch',
+    );
+  });
 });
